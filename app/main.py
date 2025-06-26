05085238ce81e71acfb2b449ce528b31416d45b4
viannik <ivan.nikitski@gmail.com>
2025-06-27 02:26:20 +0300

    Add Animal, Cat, and Dog classes with feeding functionality

--- app/main.py
+++ app/main.py
@@ -1,1 +1,43 @@
-#  write your code here
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Animal:
+    name: str
+    appetite: int
+    is_hungry: bool = True
+
+    def print_name(self)-> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self)-> int:
+        if self.is_hungry:
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            return self.appetite
+        return 0
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True):
+        super().__init__(name, 3, is_hungry)
+
+    @staticmethod
+    def catch_mouse() -> None:
+        print(f"The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True):
+        super().__init__(name, 7, is_hungry)
+
+    @staticmethod
+    def bring_slippers() -> None:
+        print(f"The slippers delivered!")
+
+
+def feed_animals(animals: List[Animal]) -> int:
+    return sum(animal.feed() for animal in animals)
