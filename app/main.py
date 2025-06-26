8dd5630eceef7a94f66ff383760c70beb104dd1b
viannik <ivan.nikitski@gmail.com>
2025-06-27 02:30:51 +0300

    Refactor method signatures for consistency and clarity

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ class Animal:
     appetite: int
     is_hungry: bool = True
 
-    def print_name(self)-> None:
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self)-> int:
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -22,21 +22,21 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
-        print(f"The hunt began!")
+        print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 7, is_hungry)
 
     @staticmethod
     def bring_slippers() -> None:
-        print(f"The slippers delivered!")
+        print("The slippers delivered!")
 
 
 def feed_animals(animals: List[Animal]) -> int:
